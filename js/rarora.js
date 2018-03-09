254c4548c2aea889270800f99c1e09672184343f
Raghav Arora <agu94.raghav@gmail.com>
2018-03-09 06:29:18 +0100

    Made Webpage

---
+++ js/rarora.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    var stage = 0;
+    var changeStage = function() {
+        stage = 1;
+        $('#one').animate({
+            opacity:0
+        }, 1000, function(){
+            $('#one').css("display", "none");
+            $('#two').css("display", "block");
+            $('#two').animate({
+            opacity:1}, 1000);
+        })
+    }
+    $('#main').on('mousewheel', function(e){
+        if(e.originalEvent.wheelDelta /120 > 0) {
+            console.log('scrolling up !');
+        }
+        else{
+            if (stage == 0) changeStage()
+        }
+    });
+});
+
