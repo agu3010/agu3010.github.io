a904b893bd9fa1d629bd50d850af28ca66a81f31
Raghav Arora <agu94.raghav@gmail.com>
2018-03-12 17:35:13 +0100

    Added calls to changeStage()

--- js/rarora.js
+++ js/rarora.js
@@ -1,27 +1,29 @@
 $(document).ready(function(){
     var stage = 0;
     var changeStage = function() {
-        stage = 1;
-        $('#one').animate({
-            opacity:0
-        }, 1000, function(){
-            $('#one').css('display', 'none');
-            $('#two').css('display', 'block');
-            $('#two').animate({
-            opacity:1}, 1000);
-        })
+        if (stage == 0) {
+            stage = 1;
+            $('#one').animate({
+                opacity:0
+            }, 1000, function(){
+                $('#one').css('display', 'none');
+                $('#two').css('display', 'block');
+                $('#two').animate({
+                opacity:1}, 1000);
+            })
+        }
     }
     $('#main').on('mousewheel', function(e){
-        if(e.originalEvent.wheelDelta /120 > 0) {
-        }
-        else{
-            if (stage == 0) changeStage();
-        }
+        if (!(e.originalEvent.wheelDelta /120 > 0)) changeStage();
     });
-    $('#main').on({ 'touchstart' : function(){
-        if (stage == 0) changeStage();
+    $('#main').on({ 'touchstart' : function(e){
+        if (stage == 0) {
+            e.preventDefault()
+            changeStage();
         }
-    });
+    }});
+    $('body').keypress(function(){ changeStage() });
+    setTimeout(changeStage, 3000)
     $('.r-popover').popover({html:true});
 });
 
